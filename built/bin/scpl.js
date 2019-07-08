e013a0972cf52580592baeee0c5b1c0282551a7b
pfg <omegariisthebest@gmail.com>
2019-07-07 22:22:09 -0700

    v1.1.5

--- built/bin/scpl.js
+++ built/bin/scpl.js
@@ -6,21 +6,32 @@ const scpl_1 = require("scpl");
 const fs = require("fs");
 const path = require("path");
 const chalk_1 = require("chalk");
-const argv = yargs.argv;
+const rp = require("request-promise");
+//@ts-ignore
+const qrcode = require("qrcode-terminal");
+const argv = yargs
+    .command("scpl [filename] [options]", "Compile ScPL file", yargs => yargs.positional("filename", {
+    describe: "Input ScPL file name.",
+    demand: "Input file is required"
+}))
+    .option("output", { alias: "o", describe: "Defaults to <input path>.scpl" })
+    .option("qrcode", { alias: "q", type: "boolean" })
+    .option("inverse", { alias: "c", type: "boolean" })
+    .alias("h", "help").argv;
 if (argv.h || argv.help) {
     console.log("Usage: scpl [inputfile] -o [outputfile]"); //eslint-disable-line no-console
     process.exit(1);
 }
-if (!argv.o && !argv.output) {
-    console.log("Usage: scpl [inputfile] -o [>outputfile]"); //eslint-disable-line no-console
+if (!argv._ || !argv._[0]) {
+    console.log("Usage: scpl inputfile"); //eslint-disable-line no-console
     process.exit(1);
 }
-if (!argv._ || !argv._[0]) {
-    console.log("Usage: scpl [>inputfile] -o [outputfile]"); //eslint-disable-line no-console
-    process.exit(1);
+const inputPath = path.resolve(argv._[0]);
+if (!argv.output) {
+    const pos = inputPath.lastIndexOf(".");
+    argv.output = `${inputPath.substr(0, pos < 0 ? inputPath.length : pos)}.shortcut`;
 }
 const outputPath = path.resolve((argv.o || argv.output));
-const inputPath = path.resolve(argv._[0]);
 if (argv.inverse) {
     console.log("Inverting");
     // read buffer
@@ -32,13 +43,13 @@ function throwError(filename, fileContent, error) {
     process.stdout.write("\n");
     filename = path.relative(process.cwd(), filename);
     if (error instanceof scpl_1.PositionedError) {
-        console.log(`${chalk_1.default.blue(filename)}:${chalk_1.default.yellow("" + error.start[0])}:${chalk_1.default.yellow("" + error.start[1])} - ` + error.message); //eslint-disable-line no-console
+        console.log(`${chalk_1.default.blue(filename)}:${chalk_1.default.yellow(`${error.start[0]}`)}:${chalk_1.default.yellow(`${error.start[1]}`)} - ${error.message}`); //eslint-disable-line no-console
         console.log();
         const split = fileContent.split(`\n`);
         const startPos = [error.start[0] - 1, error.start[1] - 1];
         const endPos = [error.end[0] - 1, error.end[1] - 1];
-        const num = chalk_1.default.inverse(`${error.start[0]}`) + " ";
-        const blankNum = chalk_1.default.inverse(`${" ".repeat(("" + error.start[0]).length)}`) + " ";
+        const num = `${chalk_1.default.inverse(`${error.start[0]}`)} `;
+        const blankNum = `${chalk_1.default.inverse(`${" ".repeat(`${error.start[0]}`.length)}`)} `;
         if (startPos[0] === endPos[0]) {
             // line numbers are the same
             const line = split[startPos[0]];
@@ -53,7 +64,7 @@ function throwError(filename, fileContent, error) {
         }
     }
     else {
-        console.log(`${chalk_1.default.blue(filename)}:${chalk_1.default.yellow("???")}:${chalk_1.default.yellow("???")} - ` + error.message); //eslint-disable-line no-console
+        console.log(`${chalk_1.default.blue(filename)}:${chalk_1.default.yellow("???")}:${chalk_1.default.yellow("???")} - ${error.message}`); //eslint-disable-line no-console
     }
     process.exit(1);
     throw new Error("Process did not exit");
@@ -92,5 +103,33 @@ try {
 catch (e) {
     throwError(inputPath, fileCont, e);
 }
+if (argv.qrcode) {
+    (async () => {
+        console.log("Generating QR code");
+        const response = await rp({
+            method: "POST",
+            uri: "https://file.io",
+            formData: {
+                file: {
+                    value: plist,
+                    options: {
+                        filename: path.basename(outputPath),
+                        contentType: "application/x-octet-stream"
+                    }
+                }
+            }
+        });
+        const json = JSON.parse(response);
+        const link = json.link;
+        if (!link) {
+            console.log("Error while uploading to generate qr code: ", response);
+        }
+        else {
+            qrcode.generate(link);
+        }
+    })();
+}
+else {
+    fs.writeFileSync(outputPath, plist);
+}
 console.log(`Done in ${new Date().getTime() - started}ms`); //eslint-disable-line no-console
-fs.writeFileSync(outputPath, plist);
